49a5c6764c9e6d9e6f006d89ea16046803c0d139
Phil Killewald <reallyasi9@users.noreply.github.com>
2022-03-08 17:23:36 -0500

    Remove unnecessary teams from Tournament

--- b1gbb/tournament.go
+++ b1gbb/tournament.go
@@ -3,17 +3,14 @@ package b1gbb
 var links = [...]int{7, 11, 12, 15, 16, 19, 20, 21, 22, 23, 24, 25, -1}
 
 type Tournament struct {
-	teams    [14]int
 	matchups [26]int
 }
 
 func CreateTournament() Tournament {
-	ts := [14]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 	// B1G tournament structure
 	ms := [26]int{11, 12, 10, 13, 7, 8, 4, -1, 6, 9, 5, -2, -3, 0, 3, -4, -5, 1, 2, -6, -7, -8, -9, -10, -11, -12}
 
 	return Tournament{
-		teams:    ts,
 		matchups: ms,
 	}
 }
@@ -30,5 +27,8 @@ func (t *Tournament) Teams(game int) (team1, team2 int) {
 }
 
 func (t *Tournament) SetTeam(slot int, team int) {
+	if slot < 0 || slot > len(t.matchups) {
+		return
+	}
 	t.matchups[slot] = team
 }
