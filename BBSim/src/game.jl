78aeb371269e22610d7b8d73092540981c9c70fc
--get <reallyasi9@users.noreply.github.com>
2024-03-19 19:19:18 -0400

    Rewiring games and teams to be more go-like

--- BBSim/src/game.jl
+++ BBSim/src/game.jl
@@ -1,25 +1,58 @@
-@kwdef struct GameSlot
-    quadrant::Quadrant
-    game::Int
-    team::Int
-    team_id::Int
+@enum Quadrant begin
+    NW = 1
+    SW
+    NE
+    SE
 end
 
 abstract type AbstractGame end
 
+in_quadrant(::AbstractGame, ::Quadrant) = nothing
+quadrants(::AbstractGame) = nothing
+game_number(::AbstractGame) = nothing
+teams(::AbstractGame) = nothing
+team(::AbstractGame, ::Integer) = nothing
+next(::AbstractGame) = nothing
+is_championship(::AbstractGame) = false
+is_final_four(::AbstractGame) = false
+
 struct Game <: AbstractGame
     quadrant::Quadrant
     game::Int
-    probabilities::Dict{Int,Float64}
+    teams::NTuple{2,AbstractTeam}
     next_game::AbstractGame
 end
 
+in_quadrant(g::Game, q::Quadrant) = g.quadrant == q
+quadrants(g::Game) = Quadrant[g.quadrant]
+game_number(g::Game) = g.game
+teams(g::Game) = g.teams
+team(g::Game, i::Integer) = g.teams[i]
+next(g::Game) = g.next_game
+
 struct FinalFourGame <: AbstractGame
+    quadrants::NTuple{2,Quadrant}
     game::Int
-    probabilities::Dict{Int,Float64}
+    teams::NTuple{2,AbstractTeam}
     next_game::ChampionshipGame
 end
 
+in_quadrant(g::FinalFourGame, q::Quadrant) = q ∈ g.quadrants
+quadrants(g::FinalFourGame) = Quadrant[g.quadrants...]
+game_number(g::FinalFourGame) = g.game
+teams(g::FinalFourGame) = g.teams
+team(g::FinalFourGame, i::Integer) = g.teams[i]
+next(g::FinalFourGame) = g.next_game
+is_final_four(::FinalFourGame) = true
+
 struct ChampionshipGame <: AbstractGame
-    probabilities::Dict{Int,Float64}
+    game::Int
+    teams::NTuple{2,AbstractTeam}
 end
+
+in_quadrant(::ChampionshipGame, ::Quadrant) = true
+quadrants(::ChampionshipGame) = Quadrant[NW, SW, NE, SE]
+game_number(g::ChampionshipGame) = g.game
+teams(g::ChampionshipGame) = g.teams
+team(g::ChampionshipGame, i::Integer) = g.teams[i]
+is_championship(::ChampionshipGame) = true
--- BBSim/src/team.jl
+++ BBSim/src/team.jl
@@ -1,4 +1,13 @@
-@kwdef struct Team
+abstract type AbstractTeam end
+
+id(::AbstractTeam) = nothing
+name(::AbstractTeam) = nothing
+league(::AbstractTeam) = nothing
+rating(::AbstractTeam) = nothing
+seed(::AbstractTeam) = nothing
+quadrant(::AbstractTeam) = nothing
+
+@kwdef struct Team <: AbstractTeam
     id::Int
     name::String
     league::String
@@ -8,3 +17,12 @@
 end
 
 StructTypes.StructType(::Type{Team}) = StructTypes.Struct()
+
+id(t::Team) = t.id
+name(t::Team) = t.name
+league(t::Team) = t.league
+rating(t::Team) = t.rating
+seed(t::Team) = t.seed
+quadrant(t::Team) = t.quadrant
+
+struct NullTeam <: AbstractTeam end
