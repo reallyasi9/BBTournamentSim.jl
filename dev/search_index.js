fd60d0e22e855359c8c8877530a19e5a573f71bf
Documenter.jl <documenter@juliadocs.github.io>
2025-04-08 17:21:14 +0000

    build based on 8d37252

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BBTournamentSim","category":"page"},{"location":"#BBTournamentSim","page":"Home","title":"BBTournamentSim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BBTournamentSim.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BBTournamentSim]","category":"page"}]
+}
