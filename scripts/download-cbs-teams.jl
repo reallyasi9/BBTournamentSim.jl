8471caae79ffdc617fe48e8182825f924fb06676
Phil Killewald <reallyasi9@users.noreply.github.com>
2026-03-21 14:22:30 -0400

    Fix teams parsing

--- scripts/download-cbs-teams.jl
+++ scripts/download-cbs-teams.jl
@@ -1,21 +1,19 @@
 using HTTP
 using JSON3
 using ArgParse
+using Base64
 
-const query_url = "https://picks.cbssports.com/graphql"
-const operation_name = "CentralTeamsQuery"
-
-const sports_types = Dict("ncaam" => "NCAAB", "ncaaw" => "NCAAW")
-const subsection = nothing
-const version = 1
-const query_hash = "8a220108aa5f077e329cd6de9e6aff0a9f37fbec3221ab142369d0e18717c243"
+const QUERY_URLS = Dict(
+    "ncaam" => "https://www.cbssports.com/college-basketball/ncaa-tournament/bracket/",
+    "ncaaw" => "https://www.cbssports.com/womens-college-basketball/ncaa-tournament/bracket/",
+)
 
 function parse_arguments(args)
     s = ArgParseSettings()
     @add_arg_table! s begin
         "league"
             help = "Tournament league to query (either 'ncaam' or 'ncaaw')"
-            range_tester = x -> x ∈ keys(sports_types)
+            range_tester = x -> x ∈ keys(QUERY_URLS)
             required = true
         "--outfile", "-o"
             help = "Path to local output JSON file (default: STDOUT)"
@@ -27,34 +25,31 @@ function parse_arguments(args)
 end
 
 function get_team_page(league)
-    variables = Dict(
-        "sportTypes" => [sports_types[league]],
-    )
-    extensions = Dict(
-        "persistedQuery" => Dict(
-            "version" => version,
-            "sha256Hash" => query_hash,
-        ),
-    )
-    query = Dict(
-        "operationName" => operation_name,
-        "variables" => JSON3.write(variables),
-        "extensions" => JSON3.write(extensions),
-    )
+    query_url = QUERY_URLS[league]
+    resp = HTTP.request("GET", query_url)
+    return String(resp.body)
+end
 
-    resp = HTTP.request("GET", query_url; query=query)
-    
-    json = JSON3.read(resp.body)
-    json_teams = json["data"]["teams"]
+function parse_team_page(html)
+    regex = r"define\('reduxPreloadedState', \[\], function\(\) {.*?atob\('(.*?)'"ms
+    matches = match(regex, html)
+    if isnothing(matches)
+        throw(ErrorException("unable to parse preloaded state information from HTML"))
+    end
+
+    decoded = base64decode(matches.captures[1])
+    json = JSON3.read(decoded)
+    json_teams = json["bracket"]["data"][1]["picksTeams"]
     json_dict = Dict(
-        t["id"] => t["location"] for t in json_teams
+        t["id"] => t["location"] for t in values(json_teams)
     )
     return json_dict
 end
 
-function main(args=ARGS)
+function (@main)(args)
     options = parse_arguments(args)
-    d = get_team_page(options["league"])
+    html = get_team_page(options["league"])
+    d = parse_team_page(html)
 
     if isnothing(options["outfile"])
         JSON3.pretty(d)
@@ -63,8 +58,4 @@ function main(args=ARGS)
             JSON3.pretty(f, d)
         end
     end
-end
-
-if !isinteractive()
-    main()
 end
